7f29d032ceaf9a335f30a754552d2e5a753feb7b
TaroEld <34767248+TaroEld@users.noreply.github.com>
2022-04-26 09:33:58 +0200

    refactor: improve organisation of CSS and JS to have less duplicate code and use the new popup system (#112)

--- ui/mods/msu/mod_settings/string_setting.js
+++ ui/mods/msu/mod_settings/string_setting.js
@@ -2,20 +2,27 @@ var StringSetting = function (_mod, _page, _setting, _parentDiv)
 {
 	var self = this;
 	this.data = _setting;
-	this.layout = $('<div class="string-container"/>');
+	this.layout = $('<div class="setting-container string-container"/>');
 	_parentDiv.append(this.layout);
 
-	this.title = $('<div class="title title-font-big font-bold font-color-title">' + _setting.name + '</div>');
-	this.layout.append(this.title);
+	this.titleContainer = $('<div class="setting-title-container"/>');
+	this.layout.append(this.titleContainer);
 
-	this.input = $('<input type="text" class="title-font-big font-bold font-color-brother-name string-input"/>');
+	this.title = $('<div class="title">' + _setting.name + '</div>');
+	this.titleContainer.append(this.title);
+
+	this.contentContainer = $('<div class="setting-content-container"/>');
+	this.layout.append(this.contentContainer);
+
+	this.input = $('<input type="text" class="title-font-normal font-color-brother-name string-input"/>');
+	this.contentContainer.append(this.input);
 	this.input.val(_setting.value);
 	this.input.on("change", function(){
 		self.data.value = self.input.val();
 	});
 
 
-	this.layout.append(this.input);
+
 
 	// Tooltip
 	this.title.bindTooltip({ contentType: 'ui-element', elementId: "msu-settings." + _mod.id + "." + _setting.id });
