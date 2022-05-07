9e2416da43b0309aaf6165920b8dba6c19317027
Enduriel <endur1el@protonmail.com>
2022-05-07 19:05:01 +0100

    feat: add bindFirst allowing adding Eventlisteners to the front of the queue

--- ui/mods/msu/utilities.js
+++ ui/mods/msu/utilities.js
@@ -95,3 +95,20 @@ MSU.capitalizeFirst = function ( _string )
 {
 	return _string.charAt(0).toUpperCase() + _string.slice(1);
 };
+
+// https://stackoverflow.com/a/2641047, allows binding an evenListener before other eventListeners
+$.fn.bindFirst = function(name, fn) {
+    // bind as you normally would
+    // don't want to miss out on any jQuery magic
+    this.on(name, fn);
+
+    // Thanks to a comment by @Martin, adding support for
+    // namespaced events too.
+    this.each(function() {
+        var handlers = $._data(this, 'events')[name.split('.')[0]];
+        // take out the handler we just inserted from the end
+        var handler = handlers.pop();
+        // move it at the beginning
+        handlers.splice(0, 0, handler);
+    });
+};
