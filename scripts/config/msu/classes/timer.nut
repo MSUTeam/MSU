154149debae8d639f3e911a78f16578bbbdc420c
TaroEld <34767248+TaroEld@users.noreply.github.com>
2022-12-10 17:48:14 +0100

    feat: add methods to pause and unpause to SQ and JS timer classes (#232)

--- scripts/config/msu/classes/timer.nut
+++ scripts/config/msu/classes/timer.nut
@@ -2,26 +2,63 @@
 {
 	ID = null;
 	Start = null;
+	PauseStart = null;
+	PauseIncrement = null;
 
-	constructor(_id)
+	constructor( _id )
 	{
 		this.ID = _id;
 		this.Start = ::Time.getExactTime();
+		this.PauseIncrement = 0;
 	}
 
-	function get(_msg = "", _stop = false)
+	function silentGet()
 	{
-	    local time = (::Time.getExactTime() - this.Start) * 1000;
-	    local text = format("Timer: %s %s at %f ms", this.ID, _stop ? "stopped" : "currently", time);
-	    if(_msg != "") text += " | Msg: " + _msg;
-	    ::logInfo(text);
+		local pauseIncrement = this.PauseIncrement;
+		if (this.PauseStart != null)
+			pauseIncrement += ::Time.getExactTime() - this.PauseStart;
+
+	    return (::Time.getExactTime() - this.Start - pauseIncrement) * 1000;
+	}
+
+	function get( _msg = "", _stop = false )
+	{
+		local time = this.silentGet()
+		local text = format("Timer: %s %s at %f ms", this.ID, _stop ? "stopped" : "currently", time);
+		if(_msg != "") text += " | Msg: " + _msg;
+		::logInfo(text);
 	    return time;
 	}
 
-	function stop(_msg = "")
+	function pause()
 	{
-		local time = this.get(_msg, true);
+		if (this.PauseStart != null)
+		{
+			::MSU.Mod.Debug.printWarning(format("Timer %s paused despite already being paused!", this.ID), "performance");
+		}
+		this.PauseStart = ::Time.getExactTime();
+	}
+
+	function unpause()
+	{
+		if (this.PauseStart == null)
+		{
+			::MSU.Mod.Debug.printWarning(format("Timer %s resumed despite not being paused!", this.ID), "performance");
+			return;
+		}
+		this.PauseIncrement += ::Time.getExactTime() - this.PauseStart;
+		this.PauseStart = null;
+	}
+
+	function stop( _msg = "" )
+	{
 	    delete ::MSU.Utils.Timers[this.ID];
-	    return time;
+	    return this.get(_msg, true);
+	}
+
+	function silentStop()
+	{
+		delete ::MSU.Utils.Timers[this.ID];
+		return this.silentGet();
 	}
 }
--- ui/mods/msu/utilities.js
+++ ui/mods/msu/utilities.js
@@ -190,24 +190,63 @@ MSU.TimerObject = function(_id)
 {
 	this.ID = _id;
 	this.Start = new Date();
+	this.PauseStart = null;
+	this.PauseIncrement = 0;
+}
+
+MSU.TimerObject.prototype.silentGet = function()
+{
+	var pauseIncrement = this.PauseIncrement;
+	if (this.PauseStart != null)
+		pauseIncrement += new Date().getTime() - this.PauseStart;
+	return (new Date()).getTime() - this.Start.getTime() - this.PauseIncrement
 }
 
 MSU.TimerObject.prototype.get = function(_msg, _stop)
 {
-	var end  = new Date();
-    var time = end.getTime() - this.Start.getTime();
-    var text = 'Timer: "' +  this.ID +  '" currently at ' +  time + 'ms';
-    if(_stop) text = 'Timer: "' +  this.ID +  '" stopped at ' +  time + 'ms';
-    if(_msg) text += " | Msg: " + _msg;
-    console.error(text);
-    return time;
+	// Make sure to substract paused timme
+	var time = this.silentGet();
+	var text = 'Timer: "' +  this.ID +  '" currently at ' +  time + 'ms';
+	if (_stop) text = 'Timer: "' +  this.ID +  '" stopped at ' +  time + 'ms';
+	if (_msg) text += " | Msg: " + _msg;
+	console.error(text);
+	return time;
+}
+
+MSU.TimerObject.prototype.pause = function()
+{
+	if (this.PauseStart != null && MSU.getSettingValue("mod_msu", "performanceLog"))
+	{
+		console.error("Timer " + this.ID + " paused despite already being paused!");
+	}
+	this.PauseStart = new Date();
+}
+
+MSU.TimerObject.prototype.unpause = function()
+{
+	if (this.PauseStart == null)
+	{
+		if (MSU.getSettingValue("mod_msu", "performanceLog"))
+		{
+			console.error("Timer " + this.ID + " resumed despite not being paused!");
+		}
+		return;
+	}
+	var pauseStop = new Date();
+	this.PauseIncrement += pauseStop.getTime() - this.PauseStart.getTime();
+	this.PauseStart = null;
 }
 
 MSU.TimerObject.prototype.stop = function(_msg)
 {
-	var time = this.get(_msg, true);
-    delete MSU.Utils.Timers[this.ID];
-    return time;
+	delete MSU.Utils.Timers[this.ID];
+	return this.get(_msg, true);
+}
+
+MSU.TimerObject.prototype.silentStop = function()
+{
+	delete MSU.Utils.Timers[this.ID];
+	return this.silentGet();
 }
 
 MSU.Utils.Timer = function(_id)
