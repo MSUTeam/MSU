3c00ae64dbf7a72047955e8443974ce6c72cc1c0
taroeld <tarob@live.de>
2023-03-23 16:36:20 +0100

    feat: add None state and hide/reset state on main menu show/hide

--- ui/mods/msu/ui_hooks/main_menu_screen.js
+++ ui/mods/msu/ui_hooks/main_menu_screen.js
@@ -1,11 +1,14 @@
 MainMenuScreen.prototype.showMainMenuModule = function ()
 {
 	this.mMainMenuModule.show();
+	Screens.MSUPopup.setState(Screens.MSUPopup.mLastState);
 };
 
 MainMenuScreen.prototype.hideMainMenuModule = function ()
 {
 	this.mMainMenuModule.hide();
+	Screens.MSUPopup.mLastState = Screens.MSUPopup.mState;
+	Screens.MSUPopup.setState(Screens.MSUPopup.mStates.None);
 };
 
 MainMenuScreen.prototype.hideNewCampaignModule = function ()
@@ -22,3 +25,19 @@ MainMenuScreen.prototype.showNewCampaignModule = function ()
 	this.mNewCampaignModule.mCancelButton.changeButtonText("Previous");
 	this.mNewCampaignModule.mStartButton.changeButtonText("Start");
 };
+
+MSU.Hooks.MainMenuScreen_hide = MainMenuScreen.prototype.hide;
+MainMenuScreen.prototype.hide = function()
+{
+	MSU.Hooks.MainMenuScreen_hide.call(this);
+	Screens.MSUPopup.mLastState = Screens.MSUPopup.mState;
+	Screens.MSUPopup.setState(Screens.MSUPopup.mStates.None);
+}
+
+MSU.Hooks.MainMenuScreen_show = MainMenuScreen.prototype.show;
+MainMenuScreen.prototype.show = function(_animate)
+{
+	MSU.Hooks.MainMenuScreen_show.call(this, _animate);
+	Screens.MSUPopup.setState(Screens.MSUPopup.mLastState);
+}
+
