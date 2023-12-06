c0bfab43094394fa7eebb2a37982e50d009d4cc5
Enduriel <endur1el@protonmail.com>
2023-12-06 20:51:10 +0000

    refactor: remove button setting title and move text inside of button

--- ui/mods/msu/mod_settings/button_setting.js
+++ ui/mods/msu/mod_settings/button_setting.js
@@ -4,23 +4,17 @@ var ButtonSetting = function (_mod, _page, _setting, _parentDiv)
 	this.layout = $('<div class="setting-container button-container"/>');
 	_parentDiv.append(this.layout);
 
-	this.titleContainer = $('<div class="setting-title-container"/>');
-	this.layout.append(this.titleContainer);
-
-	this.title = $('<div class="title title-font-normal font-color-title">' + _setting.name + '</div>');
-	this.titleContainer.append(this.title);
-
 	this.contentContainer = $('<div class="setting-content-container"/>');
 	this.layout.append(this.contentContainer);
 
-	this.button = this.contentContainer.createTextButton(_setting.value, function ()
+	this.button = this.contentContainer.createTextButton(_setting.name, function ()
 	{
 		var ret = {
 			"modID" : _mod.id,
 			"settingID" : _setting.id
 		}
 		Screens.ModSettingsScreen.notifyBackendSettingButtonPressed(ret);
-	}, 'button-setting-button', 4);
+	}, 'msu-button button-setting-button');
 
 	if (_setting.locked)
 	{
@@ -28,12 +22,10 @@ var ButtonSetting = function (_mod, _page, _setting, _parentDiv)
 	}
 
 	// Tooltip
-	this.title.bindTooltip({ contentType: 'msu-generic', modId: MSU.ID, elementId: "ModSettings.Element.Tooltip", elementModId: _mod.id, settingsElementId: _setting.id });
 	this.button.bindTooltip({ contentType: 'msu-generic', modId: MSU.ID, elementId: "ModSettings.Element.Tooltip", elementModId: _mod.id, settingsElementId: _setting.id });
 };
 
 ButtonSetting.prototype.unbindTooltip = function ()
 {
-	this.title.unbindTooltip();
 	this.button.unbindTooltip();
 };
