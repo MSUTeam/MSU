32b377516518bee0c951443f7eef0101519f5016
Enduriel <endur1el@protonmail.com>
2024-07-10 15:05:56 +0200

    feat: add separate mb/mi tags to allow modders to use bbcode

--- ui/mods/msu/ui_hooks/xbbcode.js
+++ ui/mods/msu/ui_hooks/xbbcode.js
@@ -5,5 +5,6 @@ XBBCODE.process = function( config )
 	if ($.isEmptyObject(Screens.ModSettingsScreen.mModSettings) || !MSU.getSettingValue("mod_msu", "VanillaBBCode")) {
 		config.text = config.text.replace(/\[(b|i)\](.*?)\[\/\1\]/gm, "$2");
 	}
+	config.text = config.text.replace(/\[m(b|i)\](.*?)\[\/m\1\]/gm, "[$1]$2[\/$1]");
 	return MSU.Hooks.XBBCODE_process(config);
 }
