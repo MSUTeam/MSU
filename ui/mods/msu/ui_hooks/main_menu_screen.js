537e0b59d78408b522b86a77248a521f7766ed12
taroeld <tarob@live.de>
2025-03-20 19:54:09 +0100

    feat: adapt to new main menu panel variable names for vanilla 1.5.1

--- ui/mods/msu/ui_hooks/main_menu_screen.js
+++ ui/mods/msu/ui_hooks/main_menu_screen.js
@@ -16,9 +16,9 @@ MainMenuScreen.prototype.hideNewCampaignModule = function ()
 MainMenuScreen.prototype.showNewCampaignModule = function ()
 {
 	this.mNewCampaignModule.show();
-	this.mNewCampaignModule.mFirstPanel.removeClass('display-block').addClass('display-none');
-	this.mNewCampaignModule.mSecondPanel.removeClass('display-block').addClass('display-none');
-	this.mNewCampaignModule.mThirdPanel.addClass('display-block').removeClass('display-none');
+	this.mNewCampaignModule.mOriginSelectionPanel.removeClass('display-block').addClass('display-none');
+	this.mNewCampaignModule.mCompanyCrisisSelectionPanel.removeClass('display-block').addClass('display-none');
+	this.mNewCampaignModule.mDifficultySelectionPanel.addClass('display-block').removeClass('display-none');
 	this.mNewCampaignModule.mCancelButton.changeButtonText("Previous");
 	this.mNewCampaignModule.mStartButton.changeButtonText("Start");
 };
