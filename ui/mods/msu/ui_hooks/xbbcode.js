50652f0eb6ae90fe3e5875e4aa050b8c0815b1d6
Enduriel <endur1el@protonmail.com>
2025-05-04 04:08:12 +0100

    fix: msu bold and italic css not applying

--- ui/mods/msu/ui_hooks/xbbcode.js
+++ ui/mods/msu/ui_hooks/xbbcode.js
@@ -2,6 +2,6 @@ MSU.Hooks.XBBCODE_process = XBBCODE.process;
 XBBCODE.process = function( config )
 {
 	var ret = MSU.Hooks.XBBCODE_process(config);
-	ret.html = ret.html.replace(/(?:\[|&#91;)(mi|mb)(?:\]|&#93;)(.*?)(?:\[|&#91;)\/\1(?:\]|&#93;)/gm, '<class="msu-font-$1">$2</>');
+	ret.html = ret.html.replace(/(?:\[|&#91;)(mi|mb)(?:\]|&#93;)(.*?)(?:\[|&#91;)\/\1(?:\]|&#93;)/gm, '<span class="msu-font-$1">$2</span>');
 	return ret;
 }
