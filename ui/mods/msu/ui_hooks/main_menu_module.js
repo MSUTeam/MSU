c3a77b25470c7354b0072b7ff9b10daddff77701
LordMidas <55047920+LordMidas@users.noreply.github.com>
2026-04-26 00:12:13 -0400

    refactor: move load campaign menu module hook to separate js file

--- ui/mods/msu/ui_hooks/main_menu_module.js
+++ ui/mods/msu/ui_hooks/main_menu_module.js
@@ -38,15 +38,3 @@ MainMenuModule.prototype.createTacticalMapMenuButtons = function (_isRetreatAllo
 	MSU.Hooks.MainMenuModule_createTacticalMapMenuButtons.call(this, _isRetreatAllowed, _isQuitAllowed, _quitText);
 	this.addModOptionsButton();
 };
-
-MSU.Hooks.LoadCampaignMenuModule_addCampaignEntryToList = LoadCampaignMenuModule.prototype.addCampaignEntryToList;
-LoadCampaignMenuModule.prototype.addCampaignEntryToList = function (_data)
-{
-	MSU.Hooks.LoadCampaignMenuModule_addCampaignEntryToList.call(this, _data);
-	if (_data.isIncompatibleVersion && _data.MSU_ModIncompatibility.length !== 0)
-	{
-		var entry = this.mListScrollContainer.find('.ui-control.campaign:last');
-		console.error("binding tooltip to " + entry);
-		entry.bindTooltip({ contentType: "msu-generic", modId: MSU.ID, elementId: "LoadCampaign", errors: _data.MSU_ModIncompatibility})
-	}
-};
